de4380ef3f3ec6af0ec4712f9c1d9bdf9af21799
帅帅 <>
2023-05-07 17:30:23 +0800

    feat: 类型别名

---
+++ 第二遍/06类型别名/index.js
@@ -0,0 +1,6 @@
+var code = 200;
+console.log(code);
+/**
+ * 语法 `type`
+ * 作用是对一组类型进行 `封装`
+ */ 
---
+++ 第二遍/06类型别名/index.ts
@@ -0,0 +1,11 @@
+type StatusCodes = 200 | 201 | 400 | 500 | 502;
+
+const code: StatusCodes = 200;
+
+console.log(code);
+
+
+/**
+ * 语法 `type`
+ * 作用是对一组类型进行 `封装`
+ */
