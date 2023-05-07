e0be253c931660c20d7042fd94cbdbf0d482902a
帅帅 <>
2023-05-07 08:56:25 +0800

    feat: 函数，形参和返回值

---
+++ 第二遍/04函数/041形参和返回值/index.js
@@ -0,0 +1,8 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+var minus = function (num1, num2) {
+    return num1 - num2;
+};
+console.log(add(1, 1));
+console.log(minus(2, 1));
---
+++ 第二遍/04函数/041形参和返回值/index.ts
@@ -0,0 +1,11 @@
+function add (num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+const minus = (num1: number, num2: number): number => { 
+  return num1 - num2;
+}
+
+console.log(add(1, 1));
+console.log(minus(2, 1));
+
